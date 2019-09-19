8de9efdda6e895187c269f1560804ad421562bbe
Amruth V <amruth@xoxoday.com>
2019-09-19 12:06:23 +0530

    Chapter1 of Java basics

---
+++ JavaBasics/src/com/javabasics/Chapter1.java
@@ -0,0 +1,26 @@
+package com.javabasics;
+
+public class Chapter1 {
+	
+	//Java single line comment 
+	
+	/* 
+	 *Java multiple line comment 
+	 * */
+	
+	
+	//Java main method 1
+	public static void main(String[] args) {
+		System.out.println("This is java main method 1");
+	}
+
+	/*Java main method 2
+	 * 
+	 * But remember java can have only one main method in a class. 
+	 * The method declaration can be varied from one developer to another.
+	 * 
+	 * */
+	public static void main1(String[] abc) {
+		System.out.println("Method and variable name can be changed to any name");
+	}
+}

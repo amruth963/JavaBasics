4a92046b15e464460e6cd790345ca4872921bf34
Amruth V <amruth@xoxoday.com>
2019-09-19 17:23:04 +0530

    Added access modifiers to Chapter2

--- JavaBasics/src/com/javabasics/Chapter2.java
+++ JavaBasics/src/com/javabasics/Chapter2.java
@@ -13,6 +13,25 @@ public class Chapter2 {
 	float floatNum = (float) 123.467;
 	char oneCharacter = 'a';
 	
+	/**
+	 * Also global variables can have the access modifiers
+	 * They can be declared Public, Private and Protected based on developers need
+	 * Its not mandatory to assign a value for global variables. It can be declared where ever required within a method
+	 */
+	
+	/**
+	 * Public access modifiers is visible for all
+	 * Private access modifiers is visible only in class
+	 * Protected access modifiers is visible within the package and outside the package through child class
+	 */
+	public String globalVariable;
+	public int studentCount;
+	public float monthlySalary;
+	public char oneLetter;
+	
+	
+
+	
 	public static void main(String args[]) {
 		Chapter2 cpt2 = new Chapter2();
 		cpt2.testMethod();
@@ -25,3 +44,5 @@ public class Chapter2 {
 	
 
 }
+
+

4c076f523439ab94e2637b166de95d07d3777202
Hemanth <hemanth.balaji@yahoo.in>
2019-09-25 15:24:49 +0530

    Create LearningDataTypes.java

---
+++ JavaBasics/src/com/javabasics/LearningDataTypes.java
@@ -0,0 +1,43 @@
+package com.javabasics;
+
+public class LearningDataTypes {
+
+public static void main (String []args){
+	//Variables in Java
+	int age = 25; //to indicate a small number range
+	long price = 241375321;// to indicate large number
+	double cost = 25.1;// to ndicate a decimal number
+	
+	System.out.println(age);
+	System.out.println(price);
+	System.out.println(cost);
+	
+	//Boolean is used to indicate condition (True/False)
+	boolean answer = true;
+	System.out.println(answer);
+	
+	//Characters in java
+	
+	char name = 'H';
+	System.out.println(name);
+	
+	int a = 2;
+	int b = 3;
+	System.out.println(a>b);// boolean as a output
+	System.out.println(a+2);
+	System.out.println(b-1);
+	
+	// Incrementing and decrementing values
+	
+	int x =1;
+	x = x+1;
+	System.out.println(x);
+	x++;
+	System.out.println(x);
+	x--;
+	System.out.println(x);
+	
+	
+}
+
+}

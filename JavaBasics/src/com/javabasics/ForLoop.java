e2a2401a297c7ac7582fcc5703075995c09c85a0
Amruth V <amruth@xoxoday.com>
2019-09-26 14:44:53 +0530

    added comments for for loop

--- JavaBasics/src/com/javabasics/ForLoop.java
+++ JavaBasics/src/com/javabasics/ForLoop.java
@@ -3,6 +3,15 @@ package com.javabasics;
 public class ForLoop {
 	
 	public static void main (String [] args){
+		/**
+		 * Question
+		 * 1. Why variable i is started with 1
+		 * 2. Why i<=10? why it cant be i>=10? and what happens if you do so
+		 * 3. what happens when u remove = in condition
+		 * 4. Why num=2 any reason in ypur logic?
+		 * 5. Since its a for of for loop why num=2 and j=1? We will get the o/p but logic is bad
+		 */
+		
 		int i;
 		for(i=1;i<=10;i++){
 		System.out.println("This is a test");

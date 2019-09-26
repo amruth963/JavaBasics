a76c742621e3f6ef03818488dfda95bc974cd6de
Hemanth <hemanth.balaji@yahoo.in>
2019-09-26 12:31:51 +0530

    While loop learning

---
+++ JavaBasics/src/com/javabasics/WhileLoop.java
@@ -0,0 +1,21 @@
+package com.javabasics;
+
+public class WhileLoop {
+
+	public static void main (String [] args){
+		// The major difference between while and do while loop is that in while loop the condition is checked after initializing and then the value is incremented 
+		// where as in do while the condition is checked in the last
+		
+		int i =1;	// initializing 
+		while (i<=15){	//Condition 
+			System.out.println("I am testing while loop");
+			i++;   //Incrementing value
+		}
+		int j=1; // initializing 
+		do{
+			System.out.println("I am testing Do while loop");	
+			j++;//Incrementing value
+		}
+			while (j<=15);//Condition 
+	}
+}
